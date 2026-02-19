04387dd965b3be3a17c858fb8b64e4c68fc24200
sagar-ambaliya <devilsagar6272-art@users.noreply.github.com>
2026-02-19 16:14:26 +0530

    hello

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const connetion = mysql.createConnection(
+  {
+    database:'laravel',
+    host:'localhost',
+    user:'root',
+    password:''
+  }
+);
+app.use(express.urlencoded({
+  extended:true
+}));
+app.use(express.json());
+connetion.connect((err)=>{
+if(err) console.error("contion fail");
+else console.log("connetion insert");
+});
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname + "/index.html");
+})
+app.post('/save',(req,res)=>{
+ const {name,email} = req.body;
+ const sql ="INSERT INTO table_movie (name,email)VALUES(?,?)";
+ connetion.query(sql,[name,email],(err,result)=>{
+   if(err)
+   {
+    return res.send("insert fail");
+   }
+   else
+    {
+      res.send("insert Succefully",result);
+    }
+ });
+});
+app.listen(3000 ,()=>{
+console.log("server are connetion");
+});
+let mypromise = () => new Promise((reject,resolve)=>{
+let success = false;
+if(success)
+{
+  resolve("abhi aya bro");
+}
+else
+{
+  reject("abhi nahi bro");
+}
+});
+async function run() {
+  await delay(30000);   // 30 sec wait
+  await mypromise();
+}
+run(); 
