5bfe32663921a68cd19e25f9d87a59378bbf294f
sagar-ambaliya <devilsagar6272-art@users.noreply.github.com>
2026-02-20 15:49:03 +0530

    node crud

--- app.js
+++ app.js
@@ -1,100 +1,51 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require("mysql");
 const app = express();
-const connetion = mysql.createConnection({
-    database:'laravel',
-    host:'localhost',
-    user:'root',
-    password:''
-  });
-app.use(express.urlencoded({
-  extended:true
-}));
+const db = mysql.createConnection({
+  database: 'laravel',
+  user: 'root',
+  host: 'localhost',
+  password: ''
+});
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-connetion.connect((err)=>{
-if(err) console.error("contion fail");
-else console.log("connetion insert");
+db.connect((err) => {
+  if(err) 
+  {
+    console.error("fail");
+  } 
+  else 
+  {
+    console.log("connection done");
+  }
 });
-app.get('/',(req,res)=>{
-  res.sendFile(__dirname + "/index.html");
-})
-app.post('/save',(req,res)=>{
- const {name,email} = req.body;
- const sql ="INSERT INTO table_movie (name,email)VALUES(?,?)";
- connetion.query(sql,[name,email],(err,result)=>{
-   if(err)
-   {
-    return res.send("insert fail");
-   }
-   else
-    {
-      res.send("insert Succefully",result);
-    }
- });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
-app.listen(3000, ()=>{
-console.log("server are connetion");
-});
-let mypromise = () => new Promise((reject,resolve)=>{
-let success = false;
-
-if(success)
+app.get('/fetch',(req,res)=>{
+const sql = "SELECT * FROM table_movie";
+db.query(sql,(err,result)=>{
+if(err)
 {
-  resolve("abhi aya bro");
+ res.json({error:"error"});
 }
 else
 {
-  reject("abhi nahi bro");
+ res.json(result);
 }
 });
-async function run() 
-{
-  await delay(30000);   // 30 sec wait
-  await mypromise();
-}
-run(); 
-$("#myfortable").DataTable({
-ajax:{
- url:'/insert',
- type:'post'
-},
-columns:[
-  {data:'email'},
-  {data:'name'},
-  {data:'image',
-  render:function(data)
-  {
-      return `<img src="storage/${data}" width="60px">`;
-  }
-  },
-  {
-    data:'id',
-    render:function(data)
+});
+app.post('/save', (req, res) => {
+  const { name, email } = req.body;
+  const sql = "INSERT INTO table_movie (name,email) VALUES (?,?)";
+  db.query(sql, [name, email], (err, result) => {
+    if (err) 
     {
-      return `<button class="btn btn-dark delete" data-id="${data}">delete</button>`;
+      return res.send("insert error");
     }
-  }
-]
+    res.send("insert successfully");
+  });
 });
-$("#form").on('submit','.delete',function(e){
- e.preventDefault();
- name = $().val();
- email = $().val();
- id = $(this).data('id');
- $.ajax({
-  url:'/delete/' + id,
-  type:'get',
-  data:{name:name,email:email},
-  processData:false,
-  dataType:'json',
-  cotentType:false,
-  success:function(data)
-  {
-   console.log(data);
-  },
-  error:function(e)
-  {
-   console.log(e);
-  }
- });
+app.listen(3000, () => {
+  console.log("server running on 3000");
 });
